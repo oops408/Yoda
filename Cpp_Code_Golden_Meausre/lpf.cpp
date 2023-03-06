e38d232da5f60160cb4192d80a397ab5fad0f6c5
oops408 <68081409+oops408@users.noreply.github.com>
2023-03-06 09:13:21 -0800

    Update lpf.cpp

--- Cpp_Code_Golden_Meausre/lpf.cpp
+++ Cpp_Code_Golden_Meausre/lpf.cpp
@@ -9,98 +9,64 @@
 
  */
 
-/* Library Includes */
 #include <iostream>
 
-/* Name space includes */
-using namespace std;
-
-/** Define BYTE type which is an 8-bit unsigned quantity. */
 typedef unsigned char BYTE;
 
-/** Define hard-coded test vector. */
-BYTE y_test[25] =
-   { 100, 125, 148, 168, 184, 185, 200,
-     198, 190, 177, 159, 148, 113, 87 ,
-      63,  41,  23,  17,   2,   0,  5 ,
-      16,  32,  52,  75 };
+const BYTE y_test[] = {
+    100, 125, 148, 168, 184, 185, 200,
+    198, 190, 177, 159, 148, 113, 87,
+    63, 41, 23, 17, 2, 0, 5,
+    16, 32, 52, 75
+};
 
-/** Desired filtered output vector. */
-BYTE y_lpf[25];
+const int MAX_DATA_SIZE = 100;
 
-/** Function to do calculate the average of 4 bytes */
-BYTE avg4 ( BYTE a, BYTE b, BYTE c, BYTE d )
+BYTE y_lpf[MAX_DATA_SIZE];
+
+BYTE avg4(BYTE a, BYTE b, BYTE c, BYTE d)
 {
-    unsigned sum = (a + b + c + d);
-    sum = sum / 4;
+    BYTE sum = (a + b + c + d) / 4;
     return sum;
 }
 
-void lpf ( int      reset,
-           int      loadmem,
-           int      readmem,
-           int      dolpf,
-           unsigned i,
-           BYTE     xin,
-           BYTE&    xout )
+void lpf(int reset, int loadmem, int readmem, int dolpf, unsigned i, BYTE xin, BYTE& xout)
 {
-    static BYTE mem_raw[100];
-    static BYTE mem_lpf[100];
-    static unsigned n;
+    static BYTE mem_raw[MAX_DATA_SIZE];
+    static BYTE mem_lpf[MAX_DATA_SIZE];
+    static unsigned int n = 0;
+
     if (reset) {
         n = 0;
-    } else
-    if (loadmem) {
-       BYTE nxt = i + 1;
-       mem_raw[i] = xin;
-       if (nxt > n) n = nxt;
-    } else
-    if (readmem) {
-       xout = mem_lpf[i];
     }
+    else if (loadmem) {
+        mem_raw[i] = xin;
+        if (i >= n) {
+            n = i + 1;
+        }
+    }
+    else if (readmem) {
+        xout = mem_lpf[i];
+    }
+
     if (dolpf) {
-       int j;
-       // check for backwards wrap
-       if (i>0) j=i-1; else j=n-1;
-       // apply the filter
-       mem_lpf[i] = avg4(
-            mem_raw[j],
-            mem_raw[i],
-            mem_raw[(i+1) % n],
-            mem_raw[(i+2) % n]
-            );
+        int j = (i > 0) ? (i - 1) : (n - 1);
+        mem_lpf[i] = avg4(mem_raw[j], mem_raw[i], mem_raw[(i + 1) % n], mem_raw[(i + 2) % n]);
     }
 }
 
-/** Entry point to this application which implements the
-    LPF routine. */
 int main()
 {
-    // instantiate local variables
-    unsigned int i;
-    BYTE     temp;
-    // display welcome message
-    cout << "LPF using wrapping moving average of 4!" << endl;
+    std::cout << "LPF using wrapping moving average of 4!" << std::endl;
 
-    // reset the LPF module
-    lpf(1,0,0,0,0,0,temp);
+    lpf(1, 0, 0, 0, 0, 0, y_lpf[0]);
 
-    // load in the raw data to be filtered
-    for (i=0; i<25; i++)
-        lpf (0,1,0,0,i,y_test[i],temp);
-
-    // apply the LPF
-    for (i=0; i<25; i++)
-        lpf (0,0,0,1,i,y_test[i],temp);
-
-    // display the output (or save to a file)
-    cout << "LPF4(Y) = " << endl;
-
-    for (i=0; i<25; i++) {
-       lpf (0,0,1,0,i,0,temp);
-       cout << (int)temp << endl;
+    for (unsigned int i = 0; i < sizeof(y_test) / sizeof(y_test[0]); i++) {
+        lpf(0, 1, 0, 0, i, y_test[i], y_lpf[i]);
     }
 
-    // return success
-    return 0;
-}
+    for (unsigned int i = 0; i < sizeof(y_test) / sizeof(y_test[0]); i++) {
+        lpf(0, 0, 0, 1, i, y_test[i], y_lpf[i]);
+    }
+
+    std::cout << "LPF4(Y) = " <<
